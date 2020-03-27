432da0aeba28b96d68d1eb052b6b5b50b0961f6c
jyh <jyh6842@gmail.com>
2020-03-27 20:02:11 +0900

    iBatis 기초

--- src/iBatis/MemberIbatis/test/MemberIbatisTest.java
+++ src/iBatis/MemberIbatis/test/MemberIbatisTest.java
@@ -4,11 +4,12 @@ import java.io.IOException;
 import java.io.Reader;
 import java.nio.charset.Charset;
 import java.sql.SQLException;
-
+import java.util.List;
 
 import com.ibatis.common.resources.Resources;
 import com.ibatis.sqlmap.client.SqlMapClient;
 import com.ibatis.sqlmap.client.SqlMapClientBuilder;
+
 import iBatis.MemberVO;
 
 
@@ -50,11 +51,75 @@ public class MemberIbatisTest {
 				System.out.println("insert 작업 실패");
 			}
 			
+			// 2-2 update 작업 연습
+			System.out.println("update 작업 시작...");
+			
+			MemberVO mv2 = new MemberVO();
+			mv2.setMem_id("d001");
+			mv2.setMem_name("이순신");
+			mv2.setMem_tel("9999-9999");
+			mv2.setMem_addr("서울시 강남구 대치동");
+			
+			// update() 메서드의 반환값 => 성공한 레코드 수
+			int cnt = smc.update("memberTest.updateMember", mv2);
+			if (cnt > 0) {
+				System.out.println("udpate 작업 성공");
+			}else {
+				System.out.println("update 작업 실패!!");
+			}
+			
+			// 2-3 delete 연습
+			System.out.println("delete 작업 시작...");
+			
+			// delete에서의 반환값 : 성공한 레코드 수
+			int cnt2 = smc.delete("memberTest.deleteMember", "d001");
+			if(cnt2 > 0 ) {
+				System.out.println("delete 작업 성공");
+			}else {
+				System.out.println("delete 작성 실패");
+			}
+			
+			
+			// 2-4 select 연습
+			// 1) 응답의 결과 여러개인 경우
+			System.out.println("select 연습 시작(결과가 여러개인 경우)...");
+			List<MemberVO> memList;
+			
+			// 응답의 결과가 여러개일 경우네는 queryForList 메서드를 이용한다.
+			// 이메서드는 여러개의 레코드를 VO에 담은 후 이 VO 데이터를
+			// List 에 추가해 주는 작업을 자동으로 수행한다.
+			memList = smc.queryForList("memberTest.getMemberAll");
+			
+			for(MemberVO mv3 : memList) {
+				System.out.println("ID : " + mv3.getMem_id());
+				System.out.println("이름 : " + mv3.getMem_name());
+				System.out.println("전화 : " + mv3.getMem_tel());
+				System.out.println("주소 : " + mv3.getMem_addr());
+				System.out.println("============================");
+			}
+			System.out.println("출력 끝 ~~");
+			
+			
+			// 2-4 select 연습
+			// 2)응답 결과가 한개가 확실한 경우 => 여러개면 에러 생김, 현재 실행문에서는 생성되고 삭제되기 때문에 delete를 주석 처리 한다.
+			
+			System.out.println("selelct 연습 시작(결과가 1개인 경우)...");
+			
+			// 응답 결과가 1개가 확실할 경우에는 queryForObject 메서드를 사용한다.
+			MemberVO mv4 = (MemberVO) smc.queryForObject("memberTest.getMember", "d001");
+			System.out.println("ID : " + mv4.getMem_id());
+			System.out.println("이름 : " + mv4.getMem_name());
+			System.out.println("전화 : " + mv4.getMem_tel());
+			System.out.println("주소 : " + mv4.getMem_addr());
+			System.out.println("============================");
+			
 		}catch (IOException e) {
 			e.printStackTrace();
 		}catch(SQLException e) {
 			e.printStackTrace();
 		}
+		
+		
 
 	}
 
