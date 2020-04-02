02d4918beada74b0639a943a9b51eafe85b96825
jyh <jyh6842@gmail.com>
2020-04-02 21:27:27 +0900

    commit

--- src/homework/thread/HorseGame.java
+++ src/homework/thread/HorseGame.java
@@ -1,20 +1,15 @@
 package homework.thread;
 
 public class HorseGame {
-	{
-		Horse[] horse = new Horse[] { 
-				new Horse(horseNumber[0]), new Horse(horseNumber[1]), new Horse(horseNumber[2]),
-				new Horse(horseNumber[3]), new Horse(horseNumber[4]), new Horse(horseNumber[5]),
-				new Horse(horseNumber[6]), new Horse(horseNumber[7]), new Horse(horseNumber[8]),
-				new Horse(horseNumber[9]) };
-	}
-	static String horseNumber[] = new String[10];
-	
 
-
-	
 	public static void main(String[] args) {
-
+		
+			HorseVO[] horse = new HorseVO[10];
+			
+			for (int i = 0; i < horse.length; i++) {
+				horse[i] = new HorseVO(i+1 +"번째말");
+			}
+		
 		
 
 
@@ -22,97 +17,22 @@ public class HorseGame {
 
 }//HorseGame
 
-class Horse extends Thread{
-	private String name;
-	
+// 말에 대한 정보 및 클래스
+class HorseVO extends Thread implements Comparable<HorseVO>{
+	private String horseName;
+	private String 
 
-	private String initHorse[] = new String[50];
-	
-	Horse(String name) {
-		this.name = name;
-		initHorse[0]=">";
-		for (int i = 1; i < initHorse.length; i++) {
-			initHorse[i] ="-";
-		}
+	public HorseVO() {
+		
 	}
-
 	
+	public HorseVO(String horseName) {
+		this.horseName = horseName;
+	}
 	@Override
-	public void run() {
-
-		for (int i = 0; i < initHorse.length; i++) {
-			System.out.print(initHorse[i]);
-		}//for
-
-		System.out.println();
-		
-		for (int i = 0; i < initHorse.length-1; i++) {
-			
-		try {
-            Thread.sleep((int)(Math.random()*301 + 200));
-         }catch(InterruptedException e) {
-            e.printStackTrace();
-         }
-
-		
-			String temp = initHorse[i];
-			initHorse[i] = initHorse[i+1];
-			initHorse[i+1] = temp;
-			display();
-			
-			clear();
-		}//for
-		System.out.println();
-	}//run
-	
-	public void clear() {
-		for (int i = 0; i < 100; i++) {
-			System.out.println();
-		}
+	public int compareTo(HorseVO o) {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 	
-	public void display() {
-		System.out.println(HorseGame.horseNumber[0]);
-		System.out.println(HorseGame.horseNumber[1]);
-		System.out.println(HorseGame.horseNumber[2]);
-		System.out.println(HorseGame.horseNumber[3]);
-		System.out.println(HorseGame.horseNumber[4]);
-		System.out.println(HorseGame.horseNumber[5]);
-		System.out.println(HorseGame.horseNumber[6]);
-		System.out.println(HorseGame.horseNumber[7]);
-		System.out.println(HorseGame.horseNumber[8]);
-		System.out.println(HorseGame.horseNumber[9]);
-	}
-	
-}
-
-class HorseVO {
-	String name;
-	boolean[] position = new boolean[50];
-	
-	public HorseVO(String name) {
-		super();
-		this.name = name;
-		
-		for (int i = 0; i < position.length; i++) {
-			this.position = position;
-		}
-		
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public boolean[] getPosition() {
-		return position;
-	}
-	public void setPosition(boolean[] position) {
-		this.position = position;
-	}
-	
-	
-	
-
 }
